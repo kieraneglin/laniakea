1c2da6710efe509db14acc70985c37da9435d5a9
Kieran Eglin <kieran.eglin@gmail.com>
2017-02-24 16:31:27 -0800

    implemeneted proof-of-concept for getting file extensions

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-class Laniakea {
-  constructor {
+const fs = require('fs');
 
+class Laniakea {
+  constructor(outputDestination, sortIntoFolders = false) {
+    // sortIntoFolders dictates whether the roms will end up in folders named after their system
+    this.outputDestination = outputDestination;
+    this.sortIntoFolders = sortIntoFolders
+
+    let extensionMap = JSON.parse(fs.readFileSync('dictionaries/consoles/extensions.json', 'utf8'));
+    this.validExtensions = []
+
+    extensionMap.map((obj) => {
+      this.validExtensions = this.validExtensions.concat(obj.extension)
+    });
+
+    console.log(this.validExtensions);
+  }
+
+  rename(sourceLocation){
+
+  }
+
+  getValidExtensions(filepath){
+    
   }
 }
 
-l = new Laniakea
-l.rename
+l = new Laniakea('/Users/Kieran/Documents/Programming/rom-renamer/laniakea/test_roms/dest');
+// console.log(l.romExtensions);
