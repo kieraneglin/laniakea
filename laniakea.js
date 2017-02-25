a1ca93215b9fae9376eded364656b2a8a671c405
Kieran Eglin <kieran.eglin@gmail.com>
2017-02-24 23:29:18 -0800

    added functions to get a dictionary from a filename and a game from a hash

--- laniakea.js
+++ laniakea.js
@@ -1,28 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
+const consoles = require('./dictionaries/consoles.json');
 
-class Laniakea {
+module.exports = class Laniakea {
   constructor(outputDestination, sortIntoFolders = false) {
     // sortIntoFolders dictates whether the roms will end up in folders named after their system
     this.outputDestination = outputDestination;
     this.sortIntoFolders = sortIntoFolders;
-    this.validExtensions = this.getValidExtensions(path.join('dictionaries', 'consoles', 'extensions.json'));
+    this.validExtensions = this.getValidExtensions();
   }
 
   renameFile(sourceLocation) {
+    let dict = this.getDictionaryByFile(sourceLocation);
     let hash = this.getMD5Checksum(sourceLocation);
-    let game = getGameByHash(hash);
+    let game = this.getGameByHash(hash, dict);
     // console.log(hash);
+    console.log(game);
   }
 
   // Private
 
-  getValidExtensions(filepath) {
-    let extensionMap = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  getValidExtensions() {
     let validExtensions = [];
 
-    extensionMap.map((obj) => {
+    // Consoles as defined above as a json require
+    consoles.map((obj) => {
       // Concat to make sure it's one big array, instead of array of arrays
       validExtensions = validExtensions.concat(obj.extension);
     });
@@ -30,18 +33,24 @@ class Laniakea {
     return validExtensions;
   }
 
-  getGameByHash(hash) {}
+  getGameByHash(hash, dict) {
+    return dict.find((obj) => {
+      return obj.checksums.md5 == hash;
+    });
+  }
 
-  getDictionaryByGame(filepath) {
-    let extension = path.extname(filepath);
-    let dict;
+  getDictionaryByFile(filepath) {
+    let ext = filepath.split('.').pop();
+    let dictPath;
+    // Consoles as defined above as a json require
+    let konsole = consoles.find((obj) => {
+      // return the first console to use the file extension form above
+      return obj.extension.includes(ext);
+    });
+    
+    dictPath = path.join('dictionaries', 'games', `${konsole.dictionaryName}.json`);
 
-    // switch(extension) {
-    //   case '.nes':
-    //     dict = 'nes';
-    //     break;
-    //   case '.'
-    // };
+    return JSON.parse(fs.readFileSync(dictPath, 'utf8'));
   }
 
   getMD5Checksum(filepath) {
@@ -64,7 +73,4 @@ class Laniakea {
 
     return hash.digest('hex');
   }
-}
-
-l = new Laniakea('/Users/Kieran/Documents/Programming/rom-renamer/laniakea/test_roms/dest');
-l.renameFile('/Users/Kieran/Documents/Programming/rom-renamer/laniakea/test_roms/source/1942.nes');
+};
